6c2aa508649ac9d2b5907020f5dbcb582c85c5c6
kjminn <kjminn2001@gmail.com>
2020-12-10 20:34:41 +0900

    Complete SqLiteTest

---
+++ app/src/main/java/kr/ac/ssu/kjminn/mirimidolmanagement/MyDBHelper.java
@@ -0,0 +1,25 @@
+package kr.ac.ssu.kjminn.mirimidolmanagement;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    //DB 생성
+    public MyDBHelper(@Nullable Context context) {
+        super(context, "groupDB", null, 1);
+    }
+    //table생성
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table groupTBL(gName CHAR(20) PRIMARY KEY, gNumber INTEGER);");
+    }
+    //db가 upgrade되었을 때 기존의 table을 삭제하고 새로 생성
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists groupTBL");
+        onCreate(db);
+    }
+}
